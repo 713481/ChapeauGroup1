3a1b00b322e85006818c707383e4c2f4ddd9223b
Alex Tang <144343551+713481@users.noreply.github.com>
2024-06-03 11:20:32 +0200

    ChangedUIandReferences
    Changed UI for ordering and fixed
    project references

--- ChapeauApp/ChapeauUI/orderingForm.Designer.cs
+++ ChapeauApp/ChapeauUI/orderingForm.Designer.cs
@@ -29,39 +29,38 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(orderingForm));
-            pnlLogo = new Panel();
-            pnlHeaderBackground = new Panel();
+            pnlOrderingLogo = new Panel();
+            pnlOrderingHeaderBackground = new Panel();
             btnOrderingReturn = new Button();
-            lblTableNumber = new Label();
-            btnLunch = new Button();
-            btnDiner = new Button();
-            btnDrinks = new Button();
-            btnLunchStarters = new Button();
-            btnLunchMains = new Button();
-            btnLunchDesserts = new Button();
-            btnAddDishes = new Button();
-            btnMyOrder = new Button();
-            pnlHeaderBackground.SuspendLayout();
+            lblOrderingTableNumber = new Label();
+            btnOrderingAddDishes = new Button();
+            btnOrderingMyOrder = new Button();
+            cmbOrderingCategory = new ComboBox();
+            lvOrderingMenuItems = new ListView();
+            chDishName = new ColumnHeader();
+            chPrice = new ColumnHeader();
+            chStock = new ColumnHeader();
+            pnlOrderingHeaderBackground.SuspendLayout();
             SuspendLayout();
             // 
-            // pnlLogo
+            // pnlOrderingLogo
             // 
-            pnlLogo.BackColor = Color.White;
-            pnlLogo.BackgroundImage = (Image)resources.GetObject("pnlLogo.BackgroundImage");
-            pnlLogo.BackgroundImageLayout = ImageLayout.Stretch;
-            pnlLogo.Location = new Point(120, -6);
-            pnlLogo.Name = "pnlLogo";
-            pnlLogo.Size = new Size(132, 116);
-            pnlLogo.TabIndex = 0;
+            pnlOrderingLogo.BackColor = Color.White;
+            pnlOrderingLogo.BackgroundImage = (Image)resources.GetObject("pnlOrderingLogo.BackgroundImage");
+            pnlOrderingLogo.BackgroundImageLayout = ImageLayout.Stretch;
+            pnlOrderingLogo.Location = new Point(120, -6);
+            pnlOrderingLogo.Name = "pnlOrderingLogo";
+            pnlOrderingLogo.Size = new Size(132, 116);
+            pnlOrderingLogo.TabIndex = 0;
             // 
-            // pnlHeaderBackground
+            // pnlOrderingHeaderBackground
             // 
-            pnlHeaderBackground.BackColor = Color.White;
-            pnlHeaderBackground.Controls.Add(btnOrderingReturn);
-            pnlHeaderBackground.Location = new Point(-5, -3);
-            pnlHeaderBackground.Name = "pnlHeaderBackground";
-            pnlHeaderBackground.Size = new Size(391, 113);
-            pnlHeaderBackground.TabIndex = 1;
+            pnlOrderingHeaderBackground.BackColor = Color.White;
+            pnlOrderingHeaderBackground.Controls.Add(btnOrderingReturn);
+            pnlOrderingHeaderBackground.Location = new Point(-5, -3);
+            pnlOrderingHeaderBackground.Name = "pnlOrderingHeaderBackground";
+            pnlOrderingHeaderBackground.Size = new Size(391, 113);
+            pnlOrderingHeaderBackground.TabIndex = 1;
             // 
             // btnOrderingReturn
             // 
@@ -74,96 +73,69 @@
             btnOrderingReturn.TabIndex = 0;
             btnOrderingReturn.UseVisualStyleBackColor = true;
             // 
-            // lblTableNumber
+            // lblOrderingTableNumber
             // 
-            lblTableNumber.AutoSize = true;
-            lblTableNumber.Font = new Font("Arial Narrow", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            lblTableNumber.ForeColor = SystemColors.ControlLightLight;
-            lblTableNumber.Location = new Point(27, 113);
-            lblTableNumber.Name = "lblTableNumber";
-            lblTableNumber.Size = new Size(57, 23);
-            lblTableNumber.TabIndex = 2;
-            lblTableNumber.Text = "Table: ";
+            lblOrderingTableNumber.AutoSize = true;
+            lblOrderingTableNumber.Font = new Font("Arial Narrow", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblOrderingTableNumber.ForeColor = SystemColors.ControlLightLight;
+            lblOrderingTableNumber.Location = new Point(27, 113);
+            lblOrderingTableNumber.Name = "lblOrderingTableNumber";
+            lblOrderingTableNumber.Size = new Size(57, 23);
+            lblOrderingTableNumber.TabIndex = 2;
+            lblOrderingTableNumber.Text = "Table: ";
             // 
-            // btnLunch
+            // btnOrderingAddDishes
             // 
-            btnLunch.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            btnLunch.Location = new Point(-5, 139);
-            btnLunch.Name = "btnLunch";
-            btnLunch.Size = new Size(132, 58);
-            btnLunch.TabIndex = 3;
-            btnLunch.Text = "Lunch";
-            btnLunch.UseVisualStyleBackColor = true;
+            btnOrderingAddDishes.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOrderingAddDishes.Location = new Point(27, 714);
+            btnOrderingAddDishes.Name = "btnOrderingAddDishes";
+            btnOrderingAddDishes.Size = new Size(132, 58);
+            btnOrderingAddDishes.TabIndex = 9;
+            btnOrderingAddDishes.Text = "Add";
+            btnOrderingAddDishes.UseVisualStyleBackColor = true;
             // 
-            // btnDiner
+            // btnOrderingMyOrder
             // 
-            btnDiner.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDiner.Location = new Point(120, 139);
-            btnDiner.Name = "btnDiner";
-            btnDiner.Size = new Size(132, 58);
-            btnDiner.TabIndex = 4;
-            btnDiner.Text = "Diner";
-            btnDiner.UseVisualStyleBackColor = true;
+            btnOrderingMyOrder.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOrderingMyOrder.Location = new Point(219, 714);
+            btnOrderingMyOrder.Name = "btnOrderingMyOrder";
+            btnOrderingMyOrder.Size = new Size(132, 58);
+            btnOrderingMyOrder.TabIndex = 10;
+            btnOrderingMyOrder.Text = "My order";
+            btnOrderingMyOrder.UseVisualStyleBackColor = true;
             // 
-            // btnDrinks
+            // cmbOrderingCategory
             // 
-            btnDrinks.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDrinks.Location = new Point(251, 139);
-            btnDrinks.Name = "btnDrinks";
-            btnDrinks.Size = new Size(135, 58);
-            btnDrinks.TabIndex = 5;
-            btnDrinks.Text = "Drinks";
-            btnDrinks.UseVisualStyleBackColor = true;
+            cmbOrderingCategory.FormattingEnabled = true;
+            cmbOrderingCategory.Location = new Point(27, 139);
+            cmbOrderingCategory.Name = "cmbOrderingCategory";
+            cmbOrderingCategory.Size = new Size(121, 23);
+            cmbOrderingCategory.TabIndex = 11;
             // 
-            // btnLunchStarters
+            // lvOrderingMenuItems
             // 
-            btnLunchStarters.Font = new Font("Arial", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            btnLunchStarters.Location = new Point(-5, 194);
-            btnLunchStarters.Name = "btnLunchStarters";
-            btnLunchStarters.Size = new Size(132, 39);
-            btnLunchStarters.TabIndex = 6;
-            btnLunchStarters.Text = "Starters";
-            btnLunchStarters.UseVisualStyleBackColor = true;
+            lvOrderingMenuItems.Columns.AddRange(new ColumnHeader[] { chDishName, chPrice, chStock });
+            lvOrderingMenuItems.Location = new Point(27, 177);
+            lvOrderingMenuItems.Name = "lvOrderingMenuItems";
+            lvOrderingMenuItems.Size = new Size(324, 179);
+            lvOrderingMenuItems.TabIndex = 12;
+            lvOrderingMenuItems.UseCompatibleStateImageBehavior = false;
+            lvOrderingMenuItems.View = View.Details;
             // 
-            // btnLunchMains
+            // chDishName
             // 
-            btnLunchMains.Font = new Font("Arial", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            btnLunchMains.Location = new Point(120, 194);
-            btnLunchMains.Name = "btnLunchMains";
-            btnLunchMains.Size = new Size(132, 39);
-            btnLunchMains.TabIndex = 7;
-            btnLunchMains.Text = "Mains";
-            btnLunchMains.UseVisualStyleBackColor = true;
+            chDishName.Text = "Dish name";
+            chDishName.Width = 200;
             // 
-            // btnLunchDesserts
+            // chPrice
             // 
-            btnLunchDesserts.Font = new Font("Arial", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            btnLunchDesserts.Location = new Point(251, 194);
-            btnLunchDesserts.Name = "btnLunchDesserts";
-            btnLunchDesserts.Size = new Size(135, 39);
-            btnLunchDesserts.TabIndex = 8;
-            btnLunchDesserts.Text = "Desserts";
-            btnLunchDesserts.UseVisualStyleBackColor = true;
+            chPrice.Text = "Price";
+            chPrice.Width = 70;
             // 
-            // btnAddDishes
+            // chStock
             // 
-            btnAddDishes.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            btnAddDishes.Location = new Point(27, 714);
-            btnAddDishes.Name = "btnAddDishes";
-            btnAddDishes.Size = new Size(132, 58);
-            btnAddDishes.TabIndex = 9;
-            btnAddDishes.Text = "Add";
-            btnAddDishes.UseVisualStyleBackColor = true;
-            // 
-            // btnMyOrder
-            // 
-            btnMyOrder.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            btnMyOrder.Location = new Point(219, 714);
-            btnMyOrder.Name = "btnMyOrder";
-            btnMyOrder.Size = new Size(132, 58);
-            btnMyOrder.TabIndex = 10;
-            btnMyOrder.Text = "My order";
-            btnMyOrder.UseVisualStyleBackColor = true;
+            chStock.Text = "Stock";
+            chStock.Width = 50;
             // 
             // orderingForm
             // 
@@ -171,38 +143,33 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MediumSlateBlue;
             ClientSize = new Size(384, 800);
-            Controls.Add(btnMyOrder);
-            Controls.Add(btnAddDishes);
-            Controls.Add(btnLunchDesserts);
-            Controls.Add(btnLunchMains);
-            Controls.Add(btnLunchStarters);
-            Controls.Add(btnDrinks);
-            Controls.Add(btnDiner);
-            Controls.Add(btnLunch);
-            Controls.Add(lblTableNumber);
-            Controls.Add(pnlLogo);
-            Controls.Add(pnlHeaderBackground);
+            Controls.Add(lvOrderingMenuItems);
+            Controls.Add(cmbOrderingCategory);
+            Controls.Add(btnOrderingMyOrder);
+            Controls.Add(btnOrderingAddDishes);
+            Controls.Add(lblOrderingTableNumber);
+            Controls.Add(pnlOrderingLogo);
+            Controls.Add(pnlOrderingHeaderBackground);
             Name = "orderingForm";
             Text = "Ordering";
             Load += orderingForm_Load;
-            pnlHeaderBackground.ResumeLayout(false);
+            pnlOrderingHeaderBackground.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Panel pnlLogo;
-        private Panel pnlHeaderBackground;
+        private Panel pnlOrderingLogo;
+        private Panel pnlOrderingHeaderBackground;
         private Button btnOrderingReturn;
-        private Label lblTableNumber;
-        private Button btnLunch;
-        private Button btnDiner;
-        private Button btnDrinks;
-        private Button btnLunchStarters;
-        private Button btnLunchMains;
-        private Button btnLunchDesserts;
-        private Button btnAddDishes;
-        private Button btnMyOrder;
+        private Label lblOrderingTableNumber;
+        private Button btnOrderingAddDishes;
+        private Button btnOrderingMyOrder;
+        private ComboBox cmbOrderingCategory;
+        private ListView lvOrderingMenuItems;
+        private ColumnHeader chDishName;
+        private ColumnHeader chPrice;
+        private ColumnHeader chStock;
     }
 }
